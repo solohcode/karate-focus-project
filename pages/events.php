ed5fae7006e2b49941f8528ebbcd9939ef9f49ac
solomon-ayodele65 <solomonayodele65@gmail.com>
2021-06-22 16:08:33 +0100

    commit

---
+++ pages/events.php
@@ -0,0 +1,125 @@
+<!-- include head  -->
+<?php
+include('../components/navBar/pageNavBar.php');
+?>
+
+
+<div class="mt-3 fs-2 fst-italic text-center text-underline">
+    <p>Karate Focus Events</p>
+</div>
+
+
+<div class="w-100 bg-light ">
+  <div class="">
+    <br/>
+    <div class="container my-md-5">
+      <div class="row">
+        <div class="col-md-6 d-none d-md-block">
+          <img src="../images/flex/about.jpg" alt="about" style="width:100%;" />
+        </div>
+        <div class="col-md-6">
+          <p class="fs-2 fst-italic">Karate Focus</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>updated april 29 2017</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="">
+    <br/>
+    <div class="container my-md-5">
+      <div class="row">
+          <div class="col-md-6">
+              <p class="fs-2 fst-italic">Karate Focus</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>updated april 29 2017</p>
+            </div>
+            <div class="col-md-6 d-none d-md-block">
+              <img src="../images/flex/news1.jpg" alt="about" style="width:100%;" />
+            </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="">
+    <br/>
+    <div class="container my-md-5">
+      <div class="row">
+        <div class="col-md-6 d-none d-md-block">
+          <img src="../images/flex/news2.jpg" alt="about" style="width:100%;" />
+        </div>
+        <div class="col-md-6">
+          <p class="fs-2 fst-italic">Karate Focus</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>updated april 29 2017</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="">
+    <br/>
+    <div class="container my-md-5">
+      <div class="row">
+          <div class="col-md-6">
+              <p class="fs-2 fst-italic">Karate Focus</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+              <p>updated april 29 2017</p>
+            </div>
+            <div class="col-md-6 d-none d-md-block">
+              <img src="../images/flex/news3.jpg" alt="about" style="width:100%;" />
+            </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="">
+    <br/>
+    <div class="container my-md-5">
+      <div class="row">
+        <div class="col-md-6 d-none d-md-block">
+          <img src="../images/flex/news4.jpg" alt="about" style="width:100%;" />
+        </div>
+        <div class="col-md-6">
+          <p class="fs-2 fst-italic">Karate Focus</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea distinctio impedit dolor pariatur, quaerat illo laudantium nobis tempora sit reiciendis deleniti laboriosam aliquid ab atque iusto harum cumque esse?</p>
+          <p>updated april 29 2017</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<?php
+ include('../components/footer/compFooter.php')
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
