976e0524891134606aa2c64f7f5b4ef1e7b5ac45
solomon-ayodele65 <solomonayodele65@gmail.com>
2021-06-28 11:13:53 +0100

    commit

--- components/navBar/blog/subBar.php
+++ components/navBar/blog/subBar.php
@@ -1,29 +1,12 @@
 
       <header role="banner">
-        <!-- <div class="top-bar">
-          <div class="container">
-            <div class="row">
-              <div class="col-9 social">
-                <a href="#"><span class="fa fa-twitter"></span></a>
-                <a href="#"><span class="fa fa-facebook"></span></a>
-                <a href="#"><span class="fa fa-instagram"></span></a>
-                <a href="#"><span class="fa fa-youtube-play"></span></a>
-              </div>
-              <div class="col-3 search-top">
-                <form action="#" class="search-top-form">
-                  <span class="icon fa fa-search"></span>
-                  <input type="text" id="s" placeholder="Type keyword to search...">
-                </form>
-              </div>
-            </div>
-          </div>
-        </div> -->
+      
 
         <div class="container logo-wrap">
           <div class="row pt-5">
             <div class="col-12 text-center">
               <a class="absolute-toggle d-block d-md-none" data-toggle="collapse" href="#navbarMenu" role="button" aria-expanded="false" aria-controls="navbarMenu"><span class="burger-lines"></span></a>
-              <p class="fs-4"><a href="index.php">KF Blogs</a></p>
+              <p class="fs-4 text-dark"><a href="index.php" class="text-dark">KF Blogs</a></p>
             </div>
           </div>
         </div>
